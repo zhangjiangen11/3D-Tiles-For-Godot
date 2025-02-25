42a2fd37f7fc2f017ba3dfce31d8a56dc19668b0
Kyn21kx <nefesmeralda@gmail.com>
2025-02-25 02:27:11 -0600

    Read cache config file at startup

--- cesium_godot/Models/CesiumGDConfig.cpp
+++ cesium_godot/Models/CesiumGDConfig.cpp
@@ -3,11 +3,15 @@
 #include "error_names.hpp"
 #include "godot_cpp/classes/dir_access.hpp"
 #include "godot_cpp/classes/file_access.hpp"
+#include "godot_cpp/classes/project_settings.hpp"
 #include "godot_cpp/core/class_db.hpp"
 #include "godot_cpp/core/error_macros.hpp"
 #include "missing_functions.hpp"
+#include <fstream>
 
 constexpr const char* CACHE_PATH = "user://cache";
+
+#define CONFIG_FILE_PATH String(CACHE_PATH) + "/ion_session.dat"
 
 void CesiumGDConfig::set_access_token(const String& accessToken)
 {
@@ -20,7 +24,7 @@ void CesiumGDConfig::set_access_token(const String& accessToken)
 		return;
 	}
 	
-	Ref<FileAccess> file = open_file_access_with_err(String(CACHE_PATH) + "/ion_session.dat", FileAccess::ModeFlags::WRITE, &err);
+	Ref<FileAccess> file = open_file_access_with_err(CONFIG_FILE_PATH, FileAccess::ModeFlags::WRITE, &err);
 	if (err != Error::OK) {
 		ERR_PRINT(errMsg + REFLECT_ERR_NAME(err));
 	}
@@ -48,6 +52,25 @@ CesiumGDConfig* CesiumGDConfig::get_singleton(Node* baseNode) {
 	
 	s_instance = Godot3DTiles::AssetManipulation::find_or_create_config_node(baseNode);
 
+	// Try to read the token data
+	// Alright, so for some reason FileAccess does not work here, so we'll use std::ifstream, womp womp
+	String globalPath = ProjectSettings::get_singleton()->globalize_path(CONFIG_FILE_PATH);
+	const char* globalPathCStr = globalPath.utf8().get_data();
+	printf("Global path: %s\n", globalPathCStr);
+	std::ifstream file(globalPathCStr);
+
+	if (!file.is_open()) {
+		ERR_PRINT("Error opening cached session file, please try to connect to Cesium ION and reopen the project!");
+	}
+	    
+    std::string token(
+        (std::istreambuf_iterator<char>(file)),
+        std::istreambuf_iterator<char>()
+    );
+    	
+	file.close();
+	s_instance->set_access_token(token.c_str());
+	
 	if (s_instance == nullptr) {
 		ERR_PRINT("Failed to find / Create a CesiumConfig node, try adding it manually!");
 	}
@@ -57,10 +80,6 @@ CesiumGDConfig* CesiumGDConfig::get_singleton(Node* baseNode) {
 
 
 void CesiumGDConfig::_enter_tree() {
-	// Try to read the token data
-	Error err;
-	Ref<FileAccess> file = open_file_access_with_err(String(CACHE_PATH) + "/ion_asset.dat", FileAccess::ModeFlags::WRITE_READ, &err);
-	this->set_access_token(file->get_as_text());
 }
 
 void CesiumGDConfig::_bind_methods()
